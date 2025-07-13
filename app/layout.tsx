795a9408d5b0baa1daba5d95c9746e9fb3bfb76e
Akhilendra <akhisingh2211@gmail.com>
2025-07-13 17:21:37 +0530

    update cuelinks

--- app/layout.tsx
+++ app/layout.tsx
@@ -136,6 +136,22 @@ export default function RootLayout({
             `,
           }}
         />
+        
+        {/* CueLinks Affiliate Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var cId = '237261';
+              (function(d, t) {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/') + 'cuelinksv2.js';
+                document.getElementsByTagName('body')[0].appendChild(s);
+              }());
+            `,
+          }}
+        />
       </head>
       <body
         className={`${inter.variable} font-sans antialiased`} 
